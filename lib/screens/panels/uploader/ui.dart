37dd14d61dd691bf92a157d5fc52d7318eab1e4e
Hamidvs24 <hemidvsmmusayev@gmail.com>
2022-04-21 17:20:32 +0400

    commit

--- lib/screens/panels/uploader/ui.dart
+++ lib/screens/panels/uploader/ui.dart
@@ -19,6 +19,8 @@ class UploaderPanel extends GetView<UploaderController> {
           visible: controller.isLoading.value,
           child: const Center(child: CircularProgressIndicator()),
           replacement: ListView.separated(
+            itemCount: controller.folders.length,
+            separatorBuilder: (context, index) => const Divider(),
             itemBuilder: (context, index) => ListTile(
               leading: controller.folders[index].type == FTPEntryType.FILE
                   ? SvgPicture.asset('assets/svg/file.svg', width: 60)
@@ -26,38 +28,39 @@ class UploaderPanel extends GetView<UploaderController> {
               title: Text(controller.folders[index].name ?? 'null'),
               subtitle: Text(
                   'Son dəyişiklik: ${DateFormat('dd-MM-yyyy kk:mm').format(controller.folders[index].modifyTime ?? DateTime.now())}, '
-                      'Ölçü: ${controller.folders[index].modifyTime}'),
+                  'Ölçü: ${controller.folders[index].modifyTime}'),
               trailing: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   PopupMenuButton<Map<String, dynamic>>(
-                      tooltip: 'Digər',
-                      icon: const Icon(Icons.more_vert_rounded),
-                      onSelected: (value) {
-                        switch (value) {
-                          // case 'Sil':
-                          //   break;
-                          // case 'Adını dəyiş':
-                          //   break;
-                          // case 'Sıxışdır':
-                          //   break;
-                        }
-                      },
-                      itemBuilder: (BuildContext context) =>
-                          controller.fileOperations
-                              .map(
-                                (e) => PopupMenuItem<Map<String, dynamic>>(
-                                  value: e,
-                                  child: Row(
-                                    children: [
-                                      e['icon'],
-                                      Spaces.horizontal10,
-                                      Text(e['name']),
-                                    ],
-                                  ),
+                    tooltip: 'Digər',
+                    icon: const Icon(Icons.more_vert_rounded),
+                    onSelected: (value) {
+                      switch (value) {
+                        // case 'Sil':
+                        //   break;
+                        // case 'Adını dəyiş':
+                        //   break;
+                        // case 'Sıxışdır':
+                        //   break;
+                      }
+                    },
+                    itemBuilder: (BuildContext context) =>
+                        controller.fileOperations
+                            .map(
+                              (e) => PopupMenuItem<Map<String, dynamic>>(
+                                value: e,
+                                child: Row(
+                                  children: [
+                                    e['icon'],
+                                    Spaces.horizontal10,
+                                    Text(e['name']),
+                                  ],
                                 ),
-                              )
-                              .toList()),
+                              ),
+                            )
+                            .toList(),
+                  ),
                   TextButton.icon(
                     style:
                         TextButton.styleFrom(padding: const EdgeInsets.all(16)),
@@ -67,23 +70,7 @@ class UploaderPanel extends GetView<UploaderController> {
                   ),
                 ],
               ),
-              // children: [
-              //   Row(
-              //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //     children: [
-              //       Text('Ölçü: ${controller.folders[index].size}'),
-              //       TextButton.icon(
-              //         style: TextButton.styleFrom(padding: EdgeInsets.all(16)),
-              //         onPressed: () {},
-              //         icon: const Icon(Icons.download_rounded),
-              //         label: const Text('Yüklə'),
-              //       ),
-              //     ],
-              //)
-              //],
             ),
-            separatorBuilder: (context, index) => const Divider(),
-            itemCount: controller.folders.length,
           ),
         ),
       ),
